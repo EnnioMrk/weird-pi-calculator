38f24556b5bdd2edcf97cb10b3598084348c9460
Ennio Marke <mystical.ennio@gmail.com>
2024-10-06 14:27:46 +0200

    v1.1

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
-const sticks_amt = 100000000;
+const STICKS_AMT = 10_000_000;
+
+const BOARD_HEIGHT = 3;
+const FIRST_LINE = 1;
+const SECOND_LINE = 2;
+const LINE_DISTANCE = SECOND_LINE - FIRST_LINE;
+const RANDOM_MAX_X = BOARD_HEIGHT - LINE_DISTANCE * 2;
 
 let sticks_cross = 0;
-let sticks_dont_cross = 0;
-
-//Board x starts at 0
-const board_height = 3;
-const first_line = 1;
-const second_line = 2;
-const line_distance = second_line - first_line;
-const random_max_x = board_height - line_distance * 2;
-
-let sticks = [];
 
 function getRandomX() {
-    return line_distance + Math.random() * random_max_x;
+    return LINE_DISTANCE + Math.random() * RANDOM_MAX_X;
 }
 
-//generate random sticks
-for (let i = 0; i < sticks_amt; i++) {
+for (let i = 0; i < STICKS_AMT; i++) {
     const random_first_x = getRandomX();
     const random_rotation = Math.random() * 360;
     const random_second_x =
-        random_first_x + Math.sin(random_rotation) * line_distance;
-    sticks.push([random_first_x, random_second_x]);
+        random_first_x +
+        Math.sin((random_rotation * Math.PI) / 180) * LINE_DISTANCE;
+
+    if (
+        (random_first_x < FIRST_LINE && random_second_x > FIRST_LINE) ||
+        (random_second_x < FIRST_LINE && random_first_x > FIRST_LINE) ||
+        (random_first_x < SECOND_LINE && random_second_x > SECOND_LINE) ||
+        (random_second_x < SECOND_LINE && random_first_x > SECOND_LINE)
+    ) {
+        sticks_cross++;
+    }
 }
 
-console.log(sticks.length);
+const pi_approximation =
+    (2 * LINE_DISTANCE * STICKS_AMT) / (sticks_cross * LINE_DISTANCE);
 
-sticks.forEach(([x, x_2]) => {
-    if (
-        (x < first_line && x_2 > first_line) ||
-        (x_2 < first_line && x > first_line)
-    ) {
-        //crosses the first line
-        sticks_cross++;
-    } else if (
-        (x < second_line && x_2 > second_line) ||
-        (x_2 < second_line && x > second_line)
-    ) {
-        //crosses the second line
-        sticks_cross++;
-    } else {
-        sticks_dont_cross++;
-    }
-});
-
-console.log(`Cross: ${sticks_cross}, No Cross: ${sticks_dont_cross}`);
-
-console.log(sticks_cross / 2);
+console.log(`Total sticks: ${STICKS_AMT}`);
+console.log(`Sticks crossing: ${sticks_cross}`);
+console.log(`Approximation of pi: ${pi_approximation}`);
+console.log(`Actual pi: ${Math.PI}`);
+console.log(`Difference: ${Math.abs(pi_approximation - Math.PI)}`);
